f2d279e59fa78833ecfb3de25083a2a0d953f9e8
KodiTVR-Add-on <koditvr@gmail.com>
2021-04-09 03:07:31 +0100

    UPGRADE
    UPGRADE

---
+++ plugin.audio.KodiTVR_radio_pt/addon.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+ *  Copyright (C) 2021- By Mod KodiTVR
+ *  Copyright (C) 2020 By Mod KodiTVR
+ *  This file is part of plugin.audio.KodiTVR_radio_pt
+ *
+ *  SPDX-License-Identifier: GPL-2.0-only
+ *  See LICENSE.txt for more information.
+'''
+
+from resources.lib import plugin
+
+plugin.run()
