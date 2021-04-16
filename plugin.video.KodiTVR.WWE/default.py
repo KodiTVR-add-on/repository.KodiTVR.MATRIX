27eea2db6fd007824dc790ba0ad920e514f9b2d6
KodiTVR-Add-on <koditvr@gmail.com>
2021-04-16 09:11:47 +0100

    UPGRADE
    UPGRADE

---
+++ plugin.video.KodiTVR.WWE/default.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- 
+import sys 
+import xbmcaddon, xbmcgui, xbmcplugin 
+# Plugin Info
+
+ADDON_ID      = 'plugin.video.KodiTVR.WWE'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME    = REAL_SETTINGS.getAddonInfo('name')
+ICON          = REAL_SETTINGS.getAddonInfo('icon')
+FANART        = REAL_SETTINGS.getAddonInfo('fanart')
+
+YOUTUBE_CHANNEL_ID1 = "[COLORgreen]●[/COLOR]"
+YOUTUBE_CHANNEL_ID2 = "UCJ5v_MCY6GNUBTO8-D3XoAg"
+YOUTUBE_CHANNEL_ID3 = "[COLORgreen]●[/COLOR]"
+
+def addDir(title, url, thumbnail,fanart,folder):
+    liz=xbmcgui.ListItem(title)
+    liz.setProperty('IsPlayable', 'false')
+    liz.setInfo(type="Video", infoLabels={"label":title,"title":title} )
+    liz.setArt({'thumb':thumbnail,'fanart':FANART})
+    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz,isFolder=True)
+
+if __name__ == '__main__':
+   #addDir(title = "[COLOR red]KodiTVR[/COLOR] [COLOR skylightblue]WRESTLING[/COLOR]",url = "plugin://plugin.video.youtube/"+YOUTUBE_CHANNEL_ID1+"/",thumbnail = icon1,)
+
+   addDir(title=" [COLORgreen]●[/COLOR]",url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID1+"/",thumbnail="https://i.imgur.com/SoHE1oA.png",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True)
+   addDir(title=" [COLOR yellow]WWE[/COLOR]",url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID2+"/",thumbnail="https://yt3.ggpht.com/ytc/AAUvwnhQZApiFpRRw4X6IWEDw3Vutq7qG6prfB0MApsbus4=s88-c-k-c0x00ffffff-no-rj",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
+   addDir(title=" [COLORgreen]●[/COLOR]",url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID3+"/",thumbnail="https://i.imgur.com/SoHE1oA.png",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
+   				   
+   xbmcplugin.endOfDirectory(int(sys.argv[1]),cacheToDisc=True)
