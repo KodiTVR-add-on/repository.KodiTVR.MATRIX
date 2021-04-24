933066e394a0c3cf3672808aac8ad23c2fa1574d
KodiTVR-Add-on <koditvr@gmail.com>
2021-04-24 18:21:43 +0100

    UPGRADE
    UPGRADE

--- plugin.video.KodiTVR.WWE/default.py
+++ plugin.video.KodiTVR.WWE/default.py
@@ -11,7 +11,9 @@ FANART        = REAL_SETTINGS.getAddonInfo('fanart')
 
 YOUTUBE_CHANNEL_ID1 = "[COLORgreen]●[/COLOR]"
 YOUTUBE_CHANNEL_ID2 = "UCJ5v_MCY6GNUBTO8-D3XoAg"
-YOUTUBE_CHANNEL_ID3 = "[COLORgreen]●[/COLOR]"
+YOUTUBE_CHANNEL_ID3 = "TNAwrestling"
+YOUTUBE_CHANNEL_ID4 = "WWEFanNation"
+YOUTUBE_CHANNEL_ID5 = "[COLORgreen]●[/COLOR]"
 
 def addDir(title, url, thumbnail,fanart,folder):
     liz=xbmcgui.ListItem(title)
@@ -25,6 +27,8 @@ if __name__ == '__main__':
 
    addDir(title=" [COLORgreen]●[/COLOR]",url="plugin://plugin.video.youtube/playlist/"+YOUTUBE_CHANNEL_ID1+"/",thumbnail="https://i.imgur.com/SoHE1oA.png",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True)
    addDir(title=" [COLOR yellow]WWE[/COLOR]",url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID2+"/",thumbnail="https://yt3.ggpht.com/ytc/AAUvwnhQZApiFpRRw4X6IWEDw3Vutq7qG6prfB0MApsbus4=s88-c-k-c0x00ffffff-no-rj",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
-   addDir(title=" [COLORgreen]●[/COLOR]",url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID3+"/",thumbnail="https://i.imgur.com/SoHE1oA.png",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
+   addDir(title=" [COLOR yellow]TNA WRESTLING[/COLOR]",url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID3+"/",thumbnail="https://yt3.ggpht.com/a/AATXAJyx6Ru-5W3RBJHWe-TC9fFspjNKGlZ5PuYttg=s288-c-k-c0xffffffff-no-rj-mo",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
+   addDir(title=" [COLOR yellow]WWE[/COLOR]",url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID4+"/",thumbnail="https://yt3.ggpht.com/a-/AN66SAx_23YzwxBkenlfs35ly3_BWP_5F8Trmmx0zA=s288-mo-c-c0xffffffff-rj-k-no",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
+   addDir(title=" [COLORgreen]●[/COLOR]",url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID5+"/",thumbnail="https://i.imgur.com/SoHE1oA.png",fanart="https://i.imgur.com/AFcDQp6.jpg",folder=True )
    				   
    xbmcplugin.endOfDirectory(int(sys.argv[1]),cacheToDisc=True)
